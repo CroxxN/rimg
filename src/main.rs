7ab82556c3317e8787c0926ecc0071a89a51d006
CroxxN <saradgaihre19@gmail.com>
2022-10-30 13:33:30 +0545

    progress: working cli parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+mod directory_helper;
+pub use clap::{Parser, Subcommand};
+use std::ffi::OsString;
+
+#[derive(Parser)]
+pub struct Rimg {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Initalize a rimg repository in the current directory
+    Init,
+
+    /// add files to the repository
+    /// Hint: use "*" for wildcard
+    Add { path: OsString },
+
+    /// commit changes to the repository
+    Commit {
+        #[arg(value_name = "message", long = "message", short = 'm')]
+        message: OsString,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let commands = Rimg::parse();
+
+    match commands.command {
+        Commands::Init => {
+            println!("Found init")
+        }
+        _ => {
+            println!("Idk")
+        }
+    }
 }
